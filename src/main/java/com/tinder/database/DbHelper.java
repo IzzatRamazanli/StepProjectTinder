b39751f244a8c4f60f8dcc9358069a4557787f18
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-12 19:40:46 +0400

    jdbc impl

--- src/main/java/com/tinder/database/DbHelper.java
+++ src/main/java/com/tinder/database/DbHelper.java
@@ -1,4 +1,20 @@
 package com.tinder.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbHelper {
+    private static final String url = "jdbc:mysql://localhost:3306/tinder";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    public Connection connection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public void showErrorMessage(SQLException ex) {
+        System.out.println("Error: " + ex.getMessage());
+        System.out.println("Error code: " + ex.getErrorCode());
+    }
 }
