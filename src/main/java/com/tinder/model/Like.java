4f85a28060b6b1fd3c5c70273fdbbf6a58f92d8a
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-12 20:51:35 +0400

    project lombok

--- src/main/java/com/tinder/model/Like.java
+++ src/main/java/com/tinder/model/Like.java
@@ -1,4 +1,14 @@
 package com.tinder.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Like {
+
+    private int from;
+    private int to;
+    private boolean status;
+
 }
--- src/main/java/com/tinder/model/Message.java
+++ src/main/java/com/tinder/model/Message.java
@@ -1,4 +1,15 @@
 package com.tinder.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Message {
+    private int sender;
+    private int receiver;
+    private String content;
+    private String sendDate;
 }
