0eb7edd15a579b991b43c5e6823da9558aa7dc0a
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-12 20:31:39 +0400

    project lombok

--- src/main/java/com/tinder/TinderApplication.java
+++ src/main/java/com/tinder/TinderApplication.java
@@ -1,16 +1,19 @@
 package com.tinder;
 
+import com.tinder.model.User;
 import com.tinder.servlet.TestServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 public class TinderApplication {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler handler = new ServletContextHandler();
-        handler.addServlet(TestServlet.class, "/test");
-        server.setHandler(handler);
-        server.start();
-        server.join();
+//        Server server = new Server(8080);
+//        ServletContextHandler handler = new ServletContextHandler();
+//        handler.addServlet(TestServlet.class, "/test");
+//        server.setHandler(handler);
+//        server.start();
+//        server.join();
+
+        User user = new User("ad", "wef", "iz", "we", 0, "sds");
     }
 }
--- src/main/java/com/tinder/model/User.java
+++ src/main/java/com/tinder/model/User.java
@@ -1,4 +1,20 @@
 package com.tinder.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
+
+    private int id;
+    private final String email;
+    @Getter
+    private final String password;
+    private final String name;
+    private final String surname;
+    private final int age;
+    private final String pictureUrl;
+
+
 }
