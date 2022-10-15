1f929b2c9f7d6df713a2845ac0dcdf4c81db8d77
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-16 00:24:03 +0400

    freemarker impl

--- src/main/java/com/tinder/model/Message.java
+++ src/main/java/com/tinder/model/Message.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class Message {
     private int sender;
     private int receiver;
     private String content;
     private String sendDate;
+
+    public Message() {
+        this.sendDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 }
